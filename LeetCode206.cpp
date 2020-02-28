93b72fcccf2e4356601edeb01295f8599c410dd7
Luella1212 <1320233361@qq.com>
2020-02-28 16:48:38 +0800

    leetcode206

---
+++ LeetCode206.cpp
@@ -0,0 +1,28 @@
+/*************************************************************************
+	> File Name: LeetCode206.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 五  2/28 16:47:52 2020
+ *********************************************************************
+
+ /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    struct ListNode ret, *p, *q;
+    p = head;
+    ret.next = NULL;
+    while (p) {
+        q = p->next;
+        p->next = ret.next;
+        ret.next = p;
+        p = q;
+    }
+    return ret.next;
+}
