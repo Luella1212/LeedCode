1ba9471bceff152e9387d40e1fa7cdb7b7101391
Luella1212 <1320233361@qq.com>
2020-02-28 16:36:47 +0800

    leetcode 232

---
+++ LeetCode232.cpp
@@ -0,0 +1,104 @@
+/*************************************************************************
+	> File Name: LeetCode232.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 五  2/28 16:35:51 2020
+ ************************************************************************/
+
+typedef struct {
+    int *data;
+    int top;
+} MyStack;
+
+MyStack* myStackCreate(int size) {
+    MyStack *s = (MyStack *)malloc(sizeof(MyStack));
+    s->data = (int *)malloc(sizeof(int) * size);
+    s->top = -1;
+    return s;
+}
+
+/** Push element x to the back of queue. */
+void myStackPush(MyStack* obj, int x) {
+    obj->data[++(obj->top)] = x;
+}
+
+/** Removes the element from in front of queue and returns that element. */
+int myStackPop(MyStack* obj) {
+    return obj->data[(obj->top)--];
+}
+
+/** Get the front element. */
+int myStackTop(MyStack* obj) {
+   return obj->data[obj->top]; 
+}
+
+/** Returns whether the queue is empty. */
+bool myStackEmpty(MyStack* obj) {
+    return obj->top == -1;
+}
+
+void myStackFree(MyStack* obj) {
+    free(obj->data);
+    free(obj);
+}
+
+
+typedef struct {
+    MyStack *s1, *s2;
+} MyQueue;
+
+/** Initialize your data structure here. */
+MyQueue* myQueueCreate() {
+    int size = 1024;
+    MyQueue *q = (MyQueue *)malloc(sizeof(MyQueue));
+    q->s1 = myStackCreate(size);
+    q->s2 = myStackCreate(size);
+    return q;
+}
+
+/** Push element x to the back of queue. */
+void myQueuePush(MyQueue* obj, int x) {
+    myStackPush(obj->s1, x);
+}
+
+/** Removes the element from in front of queue and returns that element. */
+int myQueuePop(MyQueue* obj) {
+    if (myStackEmpty(obj->s2)) {
+        while (!myStackEmpty(obj->s1)) {
+            myStackPush(obj->s2, myStackPop(obj->s1));
+        }
+    }
+    return myStackPop(obj->s2);
+}
+
+/** Get the front element. */
+int myQueuePeek(MyQueue* obj) {
+    if (myStackEmpty(obj->s2)) {
+        while (!myStackEmpty(obj->s1)) {
+            myStackPush(obj->s2, myStackPop(obj->s1));
+        }
+    }
+    return myStackTop(obj->s2);
+}
+
+/** Returns whether the queue is empty. */
+bool myQueueEmpty(MyQueue* obj) {
+    return myStackEmpty(obj->s1) && myStackEmpty(obj->s2); 
+}
+
+void myQueueFree(MyQueue* obj) {
+    myStackFree(obj->s1);
+    myStackFree(obj->s2);
+    free(obj);
+    return ;
+}
+
+/**
+ * Your MyQueue struct will be instantiated and called as such:
+ * struct MyQueue* obj = myQueueCreate();
+ * myQueuePush(obj, x);
+ * int param_2 = myQueuePop(obj);
+ * int param_3 = myQueuePeek(obj);
+ * bool param_4 = myQueueEmpty(obj);
+ * myQueueFree(obj);
+ */
